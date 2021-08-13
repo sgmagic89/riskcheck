ab0ced7561ffaaa4cba3b1906080a7d3cd14568f
Suman <sumanguha1989@gmail.com>
2021-08-13 14:57:53 +0530

    Code Restructuring

--- src/app/models/eartQuakeData.model.ts
+++ src/app/models/eartQuakeData.model.ts
@@ -1,7 +1,19 @@
-export class EartQuakeParameters {
+export class EarthQuakeParameters {
     latitude: number = 0;
     longitude: number = 0; 
-    starttime: any; 
-    endtime: any;
+    starttime?: string = '2010-08-01';
+    endtime?: string = '2021-08-13';
     maxradiuskm?: number = 50;
+}
+
+export class EarthQuakeData {
+    total: number = 0;
+    min?: EarthQuake = {};
+    max?: EarthQuake = {};
+    lastEarthQuake?: EarthQuake;
+}
+
+export class EarthQuake {
+    mag?: number;
+    time?: Date;
 }
---
+++ src/app/services/hazzard.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { EarthQuakeData, EarthQuakeParameters } from '../models/eartQuakeData.model';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HazzardService {
+earthQuakeDataBaseUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
+
+constructor(private apiService: ApiService) { }
+
+public getEarthQuakeData(params: EarthQuakeParameters): Observable<EarthQuakeData> {
+  let url = this.earthQuakeDataBaseUrl + this.apiService.getQueryString(params);
+  return this.apiService.get(url).pipe(
+    switchMap((data: any) => {
+      return of(this.parseEartQuakeData(data));
+    })
+  );
+}
+
+private parseEartQuakeData(data: any): EarthQuakeData {
+  let earthQuakeData: EarthQuakeData = new EarthQuakeData();
+  if(data.features.length) {
+      let lastEartQuake = data.features.sort((a: any, b: any) => {
+        if (a.properties.time < b.properties.time) {
+          return -1;
+        }
+        if (a.properties.time > b.properties.time) {
+          return 1;
+        }
+        return 0;
+      });
+      lastEartQuake = {
+        mag: lastEartQuake[lastEartQuake.length - 1].properties.mag,
+        time: new Date(lastEartQuake[lastEartQuake.length - 1].properties.time)
+      }
+      let minMaxData = data.features.sort((a: any, b: any) => {
+        if (a.properties.mag < b.properties.mag) {
+          return -1;
+        }
+        if (a.properties.mag > b.properties.mag) {
+          return 1;
+        }
+        return 0;
+      });
+      earthQuakeData = {
+        total: minMaxData.length,
+        min: {
+          mag: minMaxData[0].properties.mag,
+          time: new Date(minMaxData[0].properties.time),
+        },
+        max: {
+          mag: minMaxData[minMaxData.length - 1].properties.mag,
+          time: new Date(minMaxData[minMaxData.length - 1].properties.time),
+        },
+        lastEarthQuake: lastEartQuake
+      };
+    }
+    return earthQuakeData;
+}
+
+}
